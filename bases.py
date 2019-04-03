7150ebef718f2450da31203b0ecbe4722114f688
Jackson Ho <mintri1199@gmail.com>
2019-04-02 17:36:28 -0700

    [Modified] Finished decode and encode

--- bases.py
+++ bases.py
@@ -18,34 +18,44 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     # assert int(digits) is int
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+
+    # Check if the characters in the string is valid with the base
+    # if any(c not in string.printable[: base - 1] for c in digits):  # Generator expression
+    #     print('Invalid characters for base: {}'.format(base))
+    #     return
     # TODO: Decode digits from binary (base 2)
     result = 0
+    digits = digits[::-1]  # Reassign the digits string in reverse order
 
-    if base == 2:  # The base is 2
-        digits = digits[::-1]  # Reassign the digits in reverse order and convert it to integer
-        for i in range(len(digits)):
-            current_bit = int(digits[i])
-            if current_bit == 1:
-                result += 2**i
-
-        return result
-
-    # TODO: Decode digits from hexadecimal (base 16)
-    if base == 16:
-        digits = digits[::-1]
-        for power in range(len(digits)):
-            power_value = 16**power
-            char = digits[power]  # Assign the character of the specific power
-            numerical_value = string.hexdigits.find(char)  # Find the numerical value of the character
-            product = power_value * numerical_value
-            result += product
-
-        return result
-
-
+    # if base == 2:  # The base is 2
+    #     digits = digits[::-1]  # Reassign the digits in reverse order and convert it to integer
+    #     for i in range(len(digits)):
+    #         current_bit = int(digits[i])
+    #         if current_bit == 1:
+    #             result += 2**i
+    #
+    #     return result
+    #
+    # # TODO: Decode digits from hexadecimal (base 16)
+    # if base == 16:
+    #     digits = digits[::-1]
+    #     for power in range(len(digits)):
+    #         power_value = 16**power
+    #         char = digits[power]  # Assign the character of the specific power
+    #         numerical_value = string.hexdigits.find(char)  # Find the numerical value of the character
+    #         product = power_value * numerical_value
+    #         result += product
+    #
+    #     return result
 
     # TODO: Decode digits from any base (2 up to 36)
-    # ...
+    for power_index in range(len(digits)):
+        power_value = base**power_index  # The result of the base in the power of the power index
+        char = digits[power_index]  # Get the character of the digits string using the power index
+        numerical_value = string.printable.find(char)  # Get the numerical value of the selected character
+        result += power_value * numerical_value  # Increment result with the product of (base**power) * numerical value
+
+    return result
 
 
 def encode(number, base):
@@ -60,68 +70,92 @@ def encode(number, base):
     if number == 0:
         return "0"
 
-    # TODO: Encode number in binary (base 2)
-    if base == 2:
-        binary_str = ''
-        current_power = 0
-        power_array = []
-        finished = False
-
-        while finished is False:
-            bit_value = 2**current_power
-            if bit_value < number:  # Current power is less than the number
-                power_array.insert(0, current_power)
-                current_power += 1
-
-            elif bit_value == number:  # Current power is equal to the number
-                power_array.insert(0, current_power)
-                finished = False
-
-            else:  # The current power is more than the number
-                finished = False
-
-        for power in power_array:
-            value = 2**power
-            if value <= number:
-                number -= value
-                binary_str += "1"
-
-            else:  # bit_value is greater than number
-                binary_str += "0"
-
-        return binary_str
+    # # TODO: Encode number in binary (base 2)
+    # if base == 2:
+    #     binary_str = ''
+    #     current_power = 0
+    #     power_array = []
+    #     finished = False
+    #
+    #     while finished is False:
+    #         bit_value = 2**current_power
+    #         if bit_value < number:  # Current power is less than the number
+    #             power_array.insert(0, current_power)
+    #             current_power += 1
+    #
+    #         elif bit_value == number:  # Current power is equal to the number
+    #             power_array.insert(0, current_power)
+    #             finished = False
+    #
+    #         else:  # The current power is more than the number
+    #             finished = False
+    #
+    #     for power in power_array:
+    #         value = 2**power
+    #         if value <= number:
+    #             number -= value
+    #             binary_str += "1"
+    #
+    #         else:  # bit_value is greater than number
+    #             binary_str += "0"
+    #
+    #     return binary_str
 
     # TODO: Encode number in hexadecimal (base 16)
-    if base == 16:
-        hex_str = ""
-        current_power = 0
-        list_of_powers = []  # List of all the possible power but it will be sorted from the greatest to lowest
-        finished = False
-        while finished is False:  # Get all the possible power for the number
-            base_value = 16**current_power
-            if base_value < number:
-                list_of_powers.insert(0, current_power)
-                current_power += 1
-
-            elif base_value == number:  # Current power is equal to the number
-                list_of_powers.insert(0, current_power)
-                finished = True
-
-            else:  # The current power is more than the number
-                finished = True
-
-        for power in list_of_powers:
-            current_value = 16**power
-            limit = int(number / current_value)  # How many of one power can go into the number
-
-            number -= current_value * limit
-
-            hex_str += string.hexdigits[limit]
-
-        return hex_str
+    # if base == 16:
+    #     hex_str = ""
+    #     current_power = 0
+    #     list_of_powers = []  # List of all the possible power but it will be sorted from the greatest to lowest
+    #     finished = False
+    #     while finished is False:  # Get all the possible power for the number
+    #         base_value = 16**current_power
+    #         if base_value < number:
+    #             list_of_powers.insert(0, current_power)
+    #             current_power += 1
+    #
+    #         elif base_value == number:  # Current power is equal to the number
+    #             list_of_powers.insert(0, current_power)
+    #             finished = True
+    #
+    #         else:  # The current power is more than the number
+    #             finished = True
+    #
+    #     for power in list_of_powers:
+    #         current_value = 16**power
+    #         limit = int(number / current_value)  # How many of one power can go into the number
+    #
+    #         number -= current_value * limit
+    #
+    #         hex_str += string.hexdigits[limit]
+    #
+    #     return hex_str
 
     # TODO: Encode number in any base (2 up to 36)
-    # ...
+    encode_str = ''
+    current_power = 0
+    finished = False
+    list_of_powers = []
+    while finished is False:  # Get all the possible power of a base for the number
+        power_value = base**current_power
+        if power_value < number:
+            list_of_powers.insert(0, current_power)
+            current_power += 1
+
+        elif power_value == number:
+            list_of_powers.insert(0, current_power)
+            finished = True
+
+        else:  # The current power made the base too big
+            finished = True
+
+    for power in list_of_powers:
+        power_value = base ** power
+        limit = int(number / power_value)
+        number -= power_value * limit
+
+        encode_str += string.printable[limit]
+
+    return encode_str
 
 
 def convert(digits, base1, base2):
@@ -159,7 +193,7 @@ def main():
         print('Converts digits from base1 to base2')
 
 
-print(decode("23ceadf", 16))
+print(decode("10", 2))
 
 # print(encode(15, 2))
 
