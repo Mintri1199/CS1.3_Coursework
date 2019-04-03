27a868005692bd06a9bbe7cfb05c6b1a1ea3f04a
Jackson Ho <mintri1199@gmail.com>
2019-04-02 22:39:06 -0700

    [Modified] Implemented fraction encoding and decoding

--- bases.py
+++ bases.py
@@ -3,7 +3,7 @@
 import string
 # Hint: Use these string constants to encode/decode hexadecimal digits and more
 # string.digits is '0123456789'
-# string.hexdigits is '0123456789abcdef   ABCDEF'
+# string.hexdigits is '0123456789abcdefpytABCDEF'
 # string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
 # string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
@@ -18,14 +18,14 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     # assert int(digits) is int
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-
     # Check if the characters in the string is valid with the base
-    # if any(c not in string.printable[: base - 1] for c in digits):  # Generator expression
+    # if any(c not in string.printable[: base] for c in digits):  # Generator expression
     #     print('Invalid characters for base: {}'.format(base))
     #     return
+
     # TODO: Decode digits from binary (base 2)
-    result = 0
-    digits = digits[::-1]  # Reassign the digits string in reverse order
+    result = 0.0
+    # digits = digits[::-1]  # Reassign the digits string in reverse order
 
     # if base == 2:  # The base is 2
     #     digits = digits[::-1]  # Reassign the digits in reverse order and convert it to integer
@@ -49,15 +49,76 @@ def decode(digits, base):
     #     return result
 
     # TODO: Decode digits from any base (2 up to 36)
-    for power_index in range(len(digits)):
-        power_value = base**power_index  # The result of the base in the power of the power index
-        char = digits[power_index]  # Get the character of the digits string using the power index
-        numerical_value = string.printable.find(char)  # Get the numerical value of the selected character
-        result += power_value * numerical_value  # Increment result with the product of (base**power) * numerical value
+    # for power_index in range(len(digits)):
+    #     power_value = base**power_index  # The result of the base in the power of the power index
+    #     char = digits[power_index]  # Get the character of the digits string using the power index
+    #     numerical_value = string.printable.find(char)  # Get the numerical value of the selected character
+    #     result += power_value * numerical_value  # Increment result with the product of (base**power) * numerical value
+    #
+    #
+    # power = 0
+    if "." in digits:
+        radix_index = digits.index(".")
+        power = len(digits[:radix_index]) - 1
+    else:
+        power = len(digits) - 1
+
+    for character in digits:
+
+        if character == '.':
+            pass
+        else:
+            char_index = string.printable.index(character)
+            result += (base ** power) * char_index
+
+            power -= 1
 
     return result
+#
+#
+# def encode(number, base):
+#     """Encode given number in base 10 to digits in given base.
+#     number: int -- integer representation of number (in base 10)
+#     base: int -- base to convert to
+#     return: str -- string representation of number (in given base)"""
+#     # Handle up to base 36 [0-9a-z]
+#     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+#     # Handle unsigned numbers only for now
+#     assert number >= 0, 'number is negative: {}'.format(number)
+#     if number == 0:
+#         return "0"
+#
+#     encode_str = ''
+#     current_power = 0
+#     finished = False
+#     list_of_powers = []
+#     string_number = str(number)
+#     whole = int(number)
+#     fraction = number - whole
+#
+#     while finished is False:  # Get all the possible power of a base for the number
+#         power_value = base**current_power
+#         if power_value < whole:
+#             list_of_powers.insert(0, current_power)
+#             current_power += 1
+#
+#         elif power_value == whole:
+#             list_of_powers.insert(0, current_power)
+#             finished = True
+#
+#         else:  # The current power made the base too big
+#             finished = True
+#
+#     for power in list_of_powers:
+#         power_value = base ** power
+#         limit = int(whole / power_value)
+#         whole -= power_value * limit
+#
+#         encode_str += string.printable[limit]
+#
+#     return encode_str
 
-
+# Encoding function from Zurich Okoren after bugs fixed by me
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
     number: int -- integer representation of number (in base 10)
@@ -67,95 +128,30 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    if number == 0:
-        return "0"
+    result = ''
+    integral = int(number)
+    fractional = number - integral
+    print(number - integral)
+    while integral > 0:
+        div_tup = divmod(integral, base)   # This was the problem you had number instead of dividend
+        integral = div_tup[0]
+        result = str(string.printable[div_tup[1]]) + result
 
-    # # TODO: Encode number in binary (base 2)
-    # if base == 2:
-    #     binary_str = ''
-    #     current_power = 0
-    #     power_array = []
-    #     finished = False
-    #
-    #     while finished is False:
-    #         bit_value = 2**current_power
-    #         if bit_value < number:  # Current power is less than the number
-    #             power_array.insert(0, current_power)
-    #             current_power += 1
-    #
-    #         elif bit_value == number:  # Current power is equal to the number
-    #             power_array.insert(0, current_power)
-    #             finished = False
-    #
-    #         else:  # The current power is more than the number
-    #             finished = False
-    #
-    #     for power in power_array:
-    #         value = 2**power
-    #         if value <= number:
-    #             number -= value
-    #             binary_str += "1"
-    #
-    #         else:  # bit_value is greater than number
-    #             binary_str += "0"
-    #
-    #     return binary_str
+    if fractional:
+        result += '.'
+        print("Initial fraction: {}".format(fractional))
+        while fractional != 0:
+            fractional *= base
+            fract_bit = int(fractional)
+            print(fractional)
+            print(fract_bit)
+            if fract_bit != 0:
+                result += string.printable[fract_bit]
+            else:
+                result += '0'
+            fractional -= fract_bit
 
-    # TODO: Encode number in hexadecimal (base 16)
-    # if base == 16:
-    #     hex_str = ""
-    #     current_power = 0
-    #     list_of_powers = []  # List of all the possible power but it will be sorted from the greatest to lowest
-    #     finished = False
-    #     while finished is False:  # Get all the possible power for the number
-    #         base_value = 16**current_power
-    #         if base_value < number:
-    #             list_of_powers.insert(0, current_power)
-    #             current_power += 1
-    #
-    #         elif base_value == number:  # Current power is equal to the number
-    #             list_of_powers.insert(0, current_power)
-    #             finished = True
-    #
-    #         else:  # The current power is more than the number
-    #             finished = True
-    #
-    #     for power in list_of_powers:
-    #         current_value = 16**power
-    #         limit = int(number / current_value)  # How many of one power can go into the number
-    #
-    #         number -= current_value * limit
-    #
-    #         hex_str += string.hexdigits[limit]
-    #
-    #     return hex_str
-
-    # TODO: Encode number in any base (2 up to 36)
-    encode_str = ''
-    current_power = 0
-    finished = False
-    list_of_powers = []
-    while finished is False:  # Get all the possible power of a base for the number
-        power_value = base**current_power
-        if power_value < number:
-            list_of_powers.insert(0, current_power)
-            current_power += 1
-
-        elif power_value == number:
-            list_of_powers.insert(0, current_power)
-            finished = True
-
-        else:  # The current power made the base too big
-            finished = True
-
-    for power in list_of_powers:
-        power_value = base ** power
-        limit = int(number / power_value)
-        number -= power_value * limit
-
-        encode_str += string.printable[limit]
-
-    return encode_str
+    return result
 
 
 def convert(digits, base1, base2):
@@ -167,14 +163,15 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+    # Check if the digits has any invalid character for base 1
+    if any(c not in string.printable[: base1] for c in digits):  # Generator expression
+        print('Invalid characters for base1: {}'.format(base1))
+        return
+
+    number = decode(digits, base1)
+    converted_str = encode(number, base2)
+
+    return converted_str
 
 
 def main():
@@ -193,11 +190,7 @@ def main():
         print('Converts digits from base1 to base2')
 
 
-print(decode("10", 2))
+if __name__ == '__main__':
+    main()
 
-# print(encode(15, 2))
-
-# print(decode('101101', 2))
-
-# if __name__ == '__main__':
-#     main()
+print(encode(581.35, 32))
